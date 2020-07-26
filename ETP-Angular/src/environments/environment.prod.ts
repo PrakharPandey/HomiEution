29d08049a0dc65b2882d6b311ab94b4b13a460ae
Prakhar <1prakharpandey1@gmail.com>
2020-07-26 20:37:34 +0530

    Adding web.config file

--- ETP-Angular/src/environments/environment.prod.ts
+++ ETP-Angular/src/environments/environment.prod.ts
@@ -1,3 +1,19 @@
 export const environment = {
-  production: true
+  production: true,
+  serviceURL: 'http://15.207.86.202:3000/'
 };
+
+export const teacher_ENV = {
+  registerAsTeacher: 'teacher/addTeacher',
+  searchTeacher: 'teacher/searchTeacher',
+  rateATeacher: 'teacher/rateTeacher',
+  viewTeacher: 'teacher/viewTeacher',
+}
+
+export const student_ENV = {
+  registerAsStudent: 'student/addStudent',
+}
+
+export const parent_ENV = {
+  registerAsParent: 'parent/addParent',
+}
